cc5e99c690fa42f5946895a20abc72dc035a86e5
SHALAKA JAVHERI <37468419+Shalakaj1234@users.noreply.github.com>
2019-10-28 20:31:58 +0530

    decimal to binary

---
+++ Decimaltobinary.c
@@ -0,0 +1,22 @@
+/*#include<stdio.h>
+int main()
+{
+      int n,i=-1;
+      int r[10];
+      printf("\nEnter a number:");
+      scanf("%d",&n);
+      while(n>0)
+      {
+            r[++i]=n%2;
+            n=n/2;
+      }
+      printf("Binary number is: ");
+      while(i>=0)
+      {
+            printf(" %d",r[i]);
+            i--;
+      }
+
+      return 0;
+}
+*/
